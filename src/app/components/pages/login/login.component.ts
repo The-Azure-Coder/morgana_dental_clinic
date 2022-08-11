428c32265804c7bda461ef00795d3377a2cf7c8a
Tyrese <jrackfinn@gmail.com>
2022-08-11 01:04:35 -0500

    login form valdated

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    'email': new FormControl('',[Validators.required,Validators.email]),
+    'password': new FormControl('',[Validators.required])
+  })
 
   constructor(private router: Router) { }
 
