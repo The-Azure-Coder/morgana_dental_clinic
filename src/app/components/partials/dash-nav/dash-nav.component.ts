f886d57bd025775b2307fff7c90987ec3d84cd00
Tyrese <jrackfinn@gmail.com>
2022-08-22 00:17:13 -0500

    dashboard navigation added

---
+++ src/app/components/partials/dash-nav/dash-nav.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dash-nav',
+  templateUrl: './dash-nav.component.html',
+  styleUrls: ['./dash-nav.component.scss']
+})
+export class DashNavComponent implements OnInit {
+
+  dashRoute(route: string) {
+    return this.router.url === route
+  }
+  
+
+  constructor(private  router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
