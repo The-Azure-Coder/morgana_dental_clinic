da80ddcee6158e1fb10b83dbb345a25f753a9f18
Tyrese <jrackfinn@gmail.com>
2022-08-26 02:35:38 -0500

    app module and routing module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,30 +11,38 @@ import { AppointmentsComponent } from './components/pages/appointments/appointme
 import { BookAppointmentComponent } from './components/pages/book-appointment/book-appointment.component';
 import { EditAppointmentComponent } from './components/pages/edit-appointment/edit-appointment.component';
 import { PatientDetailsComponent } from './components/pages/patient-details/patient-details.component';
-
-
+import { DentistlistComponent } from './components/pages/dentistlist/dentistlist.component';
+import { AddDentistComponent } from './components/pages/add-dentist/add-dentist.component';
+import { EditDentistComponent } from './components/pages/edit-dentist/edit-dentist.component';
+import { ViewAppointDetailsComponent } from './components/pages/view-appoint-details/view-appoint-details.component';
+import { ServicelistComponent } from './components/pages/servicelist/servicelist.component';
+import { AddServiceComponent } from './components/pages/add-service/add-service.component';
+import { EditServiceComponent } from './components/pages/edit-service/edit-service.component';
 
 const routes: Routes = [
-  {path: '',component:HomeComponent},
-  {path: 'login',component: LoginComponent},
-  {path: 'register',component:SignupComponent},
-  {path: 'appoint',component:AppointmentFormComponent},
-  {path: 'services/:id',component:ServiceDetailComponent },
-  {path: 'dentists/:id',component: DoctorDetailsComponent },
-  {path: 'dashboard',component:DashboardComponent},
-  {path: 'appointments',component: AppointmentsComponent },
-  {path: 'bookappoints',component:BookAppointmentComponent },
-  {path: 'bookappointments/:id',component:BookAppointmentComponent},
-  {path: 'appointEdit/:id',component:EditAppointmentComponent},
-  {path: 'patientDetails/:id',component: PatientDetailsComponent}
-
-  
-
-
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'appoint', component: AppointmentFormComponent },
+  { path: 'services/:id', component: ServiceDetailComponent },
+  { path: 'dentists/:id', component: DoctorDetailsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'appointments', component: AppointmentsComponent },
+  { path: 'bookappoints', component: BookAppointmentComponent },
+  { path: 'bookappointments/:id', component: BookAppointmentComponent },
+  { path: 'appointEdit/:id', component: EditAppointmentComponent },
+  { path: 'patientDetails/:id', component: PatientDetailsComponent },
+  { path: 'dentistList', component: DentistlistComponent },
+  { path: 'addDentist', component: AddDentistComponent },
+  { path: 'editDentist/:id', component: EditDentistComponent },
+  { path: 'viewMore/:id', component: ViewAppointDetailsComponent },
+  { path: 'serviceList', component: ServicelistComponent },
+  { path: 'addService', component: AddServiceComponent },
+  { path: 'editService/:id', component: EditServiceComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,37 +7,41 @@ import { NavBarComponent } from './components/partials/nav-bar/nav-bar.component
 import { HomeComponent } from './components/pages/home/home.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { CardSliderComponent } from './components/partials/card-slider/card-slider.component';
 import { LoginComponent } from './components/pages/login/login.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule } from '@angular/material/core';
-import {SignupComponent } from './components/pages/signup/signup.component';
-import {AppointmentFormComponent } from './components/pages/appointment-form/appointment-form.component';
-import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { SignupComponent } from './components/pages/signup/signup.component';
+import { AppointmentFormComponent } from './components/pages/appointment-form/appointment-form.component';
+import { MatStepperModule } from '@angular/material/stepper';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { ServiceDetailComponent } from './components/pages/service-detail/service-detail.component';
 import { DoctorDetailsComponent } from './components/pages/doctor-details/doctor-details.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { DashNavComponent } from './components/partials/dash-nav/dash-nav.component';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { Chart1Component } from './components/partials/chart1/chart1.component';
 import { AppointmentsComponent } from './components/pages/appointments/appointments.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BookAppointmentComponent } from './components/pages/book-appointment/book-appointment.component';
 import { EditAppointmentComponent } from './components/pages/edit-appointment/edit-appointment.component';
 import { PatientDetailsComponent } from './components/pages/patient-details/patient-details.component';
-
-
-
-
+import { DentistlistComponent } from './components/pages/dentistlist/dentistlist.component';
+import { AddDentistComponent } from './components/pages/add-dentist/add-dentist.component';
+import { EditDentistComponent } from './components/pages/edit-dentist/edit-dentist.component';
+import { Chart2Component } from './components/partials/chart2/chart2.component';
+import { ViewAppointDetailsComponent } from './components/pages/view-appoint-details/view-appoint-details.component';
+import { ServicelistComponent } from './components/pages/servicelist/servicelist.component';
+import { AddServiceComponent } from './components/pages/add-service/add-service.component';
+import { EditServiceComponent } from './components/pages/edit-service/edit-service.component';
 
 @NgModule({
   declarations: [
@@ -57,14 +61,15 @@ import { PatientDetailsComponent } from './components/pages/patient-details/pati
     AppointmentsComponent,
     BookAppointmentComponent,
     EditAppointmentComponent,
-    PatientDetailsComponent
-
-  
-    
-   
-
-  
-
+    PatientDetailsComponent,
+    DentistlistComponent,
+    AddDentistComponent,
+    EditDentistComponent,
+    Chart2Component,
+    ViewAppointDetailsComponent,
+    ServicelistComponent,
+    AddServiceComponent,
+    EditServiceComponent,
   ],
   imports: [
     BrowserModule,
@@ -83,10 +88,9 @@ import { PatientDetailsComponent } from './components/pages/patient-details/pati
     HttpClientModule,
     MatPaginatorModule,
     MatTableModule,
-    MatDialogModule
-  
+    MatDialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
