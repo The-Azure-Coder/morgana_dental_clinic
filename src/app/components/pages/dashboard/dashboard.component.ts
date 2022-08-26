75d9365200bee43354ea79b24846bbad68e5d99b
Tyrese <jrackfinn@gmail.com>
2022-08-26 02:37:59 -0500

    dashboard updated

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
-import { Component, AfterViewInit, OnInit,ViewChild } from '@angular/core';
+import { Component, AfterViewInit, OnInit, ViewChild } from '@angular/core';
 // import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Dentists } from 'src/app/models/dentist';
 import { Patients } from 'src/app/models/patient';
 import { PatientsService } from 'src/app/services/patients/patients.service';
@@ -13,89 +13,86 @@ import { MatPaginator } from '@angular/material/paginator';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit,AfterViewInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  dentists: Dentists[]=[]
-  services: Services[]=[]
-  serviceCost: any[] |  Services[]=[]
-  dentistLength!: number
-  patientLength!: number
-  serviceLength!: number
+  dentists: Dentists[] = [];
+  services: Services[] = [];
+  serviceCost: any[] | Services[] = [];
+  dentistLength!: number;
+  patientLength!: number;
+  serviceLength!: number;
 
   dentistDataSource!: MatTableDataSource<Dentists>;
   patientDataSource!: MatTableDataSource<Patients>;
   displayedColumns: string[] = ['firstname', 'lastname', 'email'];
-  patientColumns: string[] = ['firstname', 'lastname','dentist','service','cost'];
-
+  patientColumns: string[] = [
+    'firstname',
+    'lastname',
+    'dentist',
+    'service',
+    'cost',
+  ];
 
   // @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private dentistService:DentistsService, private patientService:PatientsService, private servicesService: ServicesService) {}
+  constructor(
+    private dentistService: DentistsService,
+    private patientService: PatientsService,
+    private servicesService: ServicesService
+  ) {}
 
-  
-  getAllService(){
-
+  getAllService() {
     this.servicesService.getAllServices().subscribe({
       next: (res) => {
-        console.log(res)
         // this.dentistDataSource = new MatTableDataSource(res.data);
-         this.services = res.data
-         this.serviceLength = res.data.length
-         console.log(this.dentistLength)
-        this.patientDataSource.paginator = this.paginator
+        this.services = res.data;
+        this.serviceLength = res.data.length;
+        this.patientDataSource.paginator = this.paginator;
         // this.dataSource.sort = this.sort
       },
       error: (err) => {
-        alert("Error while fetching the records")
-
-      }})
-  
-  
+        alert('Error while fetching the records');
+      },
+    });
   }
-  
-  getDentistsList(){
+
+  getDentistsList() {
     this.dentistService.getAllDentists().subscribe({
       next: (res) => {
-        console.log(res)
         // this.dentistDataSource = new MatTableDataSource(res.data);
-         this.dentists = res.data
-         this.dentistLength = res.data.length
-         console.log(this.dentistLength)
+        this.dentists = res.data;
+        this.dentistLength = res.data.length;
+
         // this.dataSource.paginator = this.paginator
         // this.dataSource.sort = this.sort
       },
       error: (err) => {
-        alert("Error while fetching the records")
-
-      }})
+        alert('Error while fetching the records');
+      },
+    });
   }
 
-  getPatientList(){
+  getPatientList() {
     this.patientService.getAllPatients().subscribe({
       next: (res) => {
-        console.log(res)
         this.patientDataSource = new MatTableDataSource(res.data);
-        this.patientLength = res.data.length
-      
-      },error: (err) => {
-        alert("Error while fetching the patients")
-      }
-    })
+        this.patientLength = res.data.length;
+      },
+      error: (err) => {
+        alert('Error while fetching the patients');
+      },
+    });
   }
 
-  ngAfterViewInit() {
-    
-  }
+  ngAfterViewInit() {}
 
   ngOnInit(): void {
-    this.getDentistsList()
-    this.getPatientList()
-    this.getAllService()
-  
+    this.getDentistsList();
+    this.getPatientList();
+    this.getAllService();
   }
-
 }
