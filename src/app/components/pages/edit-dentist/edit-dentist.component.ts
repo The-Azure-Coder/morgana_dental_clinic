f12d92894ad2160bac6eb13697268580c22d0357
Tyrese <jrackfinn@gmail.com>
2022-08-26 02:39:20 -0500

    edit details component added

---
+++ src/app/components/pages/edit-dentist/edit-dentist.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Dentists } from 'src/app/models/dentist';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { DentistsService } from 'src/app/services/doctors/doctors.service';
+
+
+@Component({
+  selector: 'app-edit-dentist',
+  templateUrl: './edit-dentist.component.html',
+  styleUrls: ['./edit-dentist.component.scss']
+})
+export class EditDentistComponent implements OnInit {
+  dentist!: Dentists;
+  dentistForm!: FormGroup;
+
+  constructor(private dentistService:DentistsService,private router: Router,private route: ActivatedRoute,) { }
+
+  updateDentist(){
+    this.dentistService.updateService(this.dentist._id,this.dentistForm.value).subscribe(()=>{
+      this.router.navigate(['/dentistList'])
+    })
+   }
+
+  ngOnInit(): void {
+
+    this.dentistService.getDentistsById(this.route.snapshot.params['id']).subscribe((results) => {
+      this.dentist = results.data;
+      this.dentistForm = new FormGroup({
+        'first_nm': new FormControl(results.data.first_nm,[Validators.required]),
+        'last_nm': new FormControl(results.data.last_nm,[Validators.required]),
+        'email': new FormControl(results.data.email,[Validators.required, Validators.email]),
+        'phoneNumber': new FormControl(results.data.phoneNumber,[Validators.required]),
+        'docDescrip': new FormControl(results.data.docDescrip,[Validators.required]),
+        'docImg': new FormControl(results.data.docImg,[Validators.required]),
+      })
+
+    
+    })
+
+
+
+
+  }
+    
+
+}
+
